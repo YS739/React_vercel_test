cf46237e92d6953cc0aad3099511d99dc58e408e
YS739 <suk921@gmail.com>
2022-12-05 21:17:46 +0900

    change 교환's age

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     { id: 1, age: 30, name: "송중기" },
     { id: 2, age: 24, name: "송강" },
     { id: 3, age: 21, name: "김유정" },
-    { id: 4, age: 29, name: "구교환" },
+    { id: 4, age: 23, name: "구교환" },
   ]);
   const [name, setName] = useState("");
   const [age, setAge] = useState("");
